8ea23345ffa0d04c23185b9918007e58d565c753
Emre Ekiz <emreekiz3455@gmail.com>
2025-10-11 23:57:49 +0300

    initial commit

---
+++ src/main/java/testRunners/Runner.java
@@ -0,0 +1,14 @@
+package testRunners;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {"src/test/java/Features"},
+        glue = {"StepDefinitions","Util"},
+        tags = "",
+        plugin = {
+                "summary","pretty","html:Reports/CucumberReport/Reports.html"
+        }
+)
+public class Runner extends AbstractTestNGCucumberTests {
+}
